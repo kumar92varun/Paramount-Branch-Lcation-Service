e04554c564bb75b0a37fc4bb75dc26cb70aa6661
Varun Kumar <kumar92varun@gmail.com>
2017-06-22 14:16:19 +0530

    PC: initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+//JaraScript Variable of type Array...
+var array_example = [];
+
+array_example[0] = 91;
+array_example[1] = 900;
+array_example[2] = 'Kartik';
+array_example[3] = 'Varun';
+array_example[4] = 6.7;
+array_example[200] = 'popup';
+
+
+
+
+
+//JavaScript variable of Object Type...
+var object_example = {};
+object_example['name'] = 'Kartik';
+object_example['age'] = 20;
+object_example['address'] = 'Shastri Nagar';
+object_example['city'] = 'New Delhi';
+object_example['state'] = 'Delhi';
+
+
+
+//JavaScript variable of Object Type...
+var json_example = {};
+var json_example = {'name':'Kartik', 'age':20, 'address':'Shastri Nagar', 'city':'New Delhi', 'state':'Delhi'};
+
+var json_example = {
+	'name':'Kartik',
+	'age':20,
+	'address':'Shastri Nagar',
+	'city':'New Delhi',
+	'state':'Delhi'
+};
+
+
+
+
+var geo_options = {
+	enableHighAccuracy: true, 
+	maximumAge        : 30000, 
+	timeout           : 27000
+};
+
+
+
+//JSON => JavaScript Object Notation
